8dd05964a72e7defef28dcebfec01942fcba7dd7
BagusAri-17 <bagusari.widhi@gmail.com>
2024-06-13 20:18:13 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
+
+const app = express()
+
+app.use(cors());
+
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+
+
+app.listen(8800, () => {
+    console.log("Connected!");
+})
